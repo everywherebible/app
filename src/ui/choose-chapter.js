ba4812ef45396689fc6ccc98c116931a8b48f9aa
Aaron Stacy <aaron.r.stacy@gmail.com>
2017-10-03 21:54:44 -0500

    fix chapter picker layout

--- src/ui/choose-chapter.js
+++ src/ui/choose-chapter.js
@@ -2,28 +2,31 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import {chapterCounts} from '../data';
+import {NAV_HEIGHT} from './nav';
 
 export default ({match: {params: {book}}, addRecent}) =>
-  <div
-      className="fit"
-      style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        textAlign: 'center',
-        overflowY: 'scroll',
-      }}>
-    {[...Array(chapterCounts[book])].map((_, i) =>
-      <Link
-          key={i}
-          to={`/${book}+${i + 1}`}
-          onClick={event => addRecent({book, chapter: i + 1, verse: 1})}
-          style={{
-            width: '2rem',
-            padding: '1rem',
-            textDecoration: 'none',
-            color: 'black',
-          }}>
-        {i + 1}
-      </Link>)}
-  </div>
+  <div className="fit" style={{overflowY: 'scroll'}}>
+    <div
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+          textAlign: 'center',
+          overflowY: 'scroll',
+          marginBottom: NAV_HEIGHT,
+        }}>
+      {[...Array(chapterCounts[book])].map((_, i) =>
+        <Link
+            key={i}
+            to={`/${book}+${i + 1}`}
+            onClick={event => addRecent({book, chapter: i + 1, verse: 1})}
+            style={{
+              width: '2rem',
+              padding: '1rem',
+              textDecoration: 'none',
+              color: 'black',
+            }}>
+          {i + 1}
+        </Link>)}
+    </div>
+  </div>;
